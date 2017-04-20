65c29c39326de618ee06163952db29798cbce230
Pavel Celut <CelutP@quantumart.ru>
2017-04-20 15:47:07 +0300

    Fix nuget

--- QA.Core.Web/Properties/AssemblyInfo.cs
+++ QA.Core.Web/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using QA.Core.Web;
 using QA.Core.Web.Properties;
 using QA.Core;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QA.Core.Web")]
@@ -18,8 +18,8 @@ using QA.Core;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -29,15 +29,15 @@ using QA.Core;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.3.0")]
-[assembly: AssemblyFileVersion("3.0.3.0")]
+[assembly: AssemblyVersion("3.0.4.0")]
+[assembly: AssemblyInformationalVersion("3.0.4.0")]
 
 // Позволяем тестировать internals
 [assembly: PreApplicationStartMethod(typeof(PreApplicationInitializer), "Start")]
